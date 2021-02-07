50723162ca49a5752e62d34a16d8667ef6e80a56
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-02-07 00:19:03 -0300

    aula 17

---
+++ aula 17 - objetoMath/index.js
@@ -0,0 +1,32 @@
+// EM JS VC PODE DIVIDIR UM NÚMERO POR ZERO
+// PODE RESULTAR EM INFINITY
+// QUANTO MAIS SE APROXIMA DO ZERO INFINITO
+
+let num1 = 9.54321;
+
+// Arredondar para baixo
+console.log(Math.floor(num1));
+
+// Arredondar para cima
+console.log(Math.ceil(num1));
+
+// Arredondar para o mais próximo
+console.log(Math.round(num1));
+
+// Maior número de uma sequencia
+console.log(Math.max(1,3,6,9,28,259));
+
+// Menor número de uma sequencia
+console.log(Math.min(1,3,6,9,28,259));
+
+// Gerar aleatório entre 0 e 1
+console.log(Math.random());
+
+// Gerar aleatório entre 10 e 5
+console.log(Math.random() * (10-5) + 5);
+
+// Potencia
+console.log(Math.pow(2, 10));
+
+// Raiz quadrada
+console.log(Math.sqrt(4));
