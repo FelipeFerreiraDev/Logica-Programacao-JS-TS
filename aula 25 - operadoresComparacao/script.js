13cad834a59c6a3b43ae5b4ab1c015941a22e763
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-02-11 20:28:41 -0300

    Aula 25

---
+++ aula 25 - operadoresComparacao/script.js
@@ -0,0 +1,11 @@
+/**
+ * Operadores de comparação
+ * > maior 
+ * >= maior ou igual
+ * < menor
+ * <= menor ou igual
+ * == igualdade (compara o conteúdo)
+ * === igualdade estrita(compara o tipo e o conteúdo)
+ * != diferente (compara o conteúdo)
+ * !== diferente estrito (compara o tipo e o conteúdo)
+ */
