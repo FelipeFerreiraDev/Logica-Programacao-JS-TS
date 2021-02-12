55ed58b1fef141f88aef7a560ee6385d3de5de40
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-02-11 21:00:09 -0300

    Aula 27

---
+++ aula 27 - curtoCircuito/script.js
@@ -0,0 +1,18 @@
+/**
+ * && -> false && true = false
+ *      false && false = true
+ * 
+ * || -> true || false = true
+ *      false || false = true
+ *      true || true = true
+ * 
+ * Valores que são falsos em JS
+ * 
+ * FLASY
+ * false
+ * 0
+ * '' "" ``
+ * null / undefined
+ * NaN
+ *
+ * */
