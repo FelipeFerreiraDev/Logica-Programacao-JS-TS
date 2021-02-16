4bac318d7d3093662e03e5384de39016d11e9ee4
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-02-16 11:32:47 -0300

    Incrementando aula 09

--- aula 09 - diferençaVarLet/scirpt.js
+++ aula 09 - diferençaVarLet/scirpt.js
@@ -6,3 +6,13 @@ console.log(nome);
 //Não gera erro com "var" já com "let" gera o erro
 
 //nome = 'Ferreira'; // Cria uma variavel global do tipo var e pode gerar bugs
+
+// Var só tem escopo em função
+// Let tem escopo de bloco ex:
+let nome3 = 'Luiz'; // criando dentro de um escopo
+var nome4 = 'Luiz';
+
+{ // Tudo que está dentro da chave é um bloco
+    let nome3 = 'Felipe'; // criando dentro de outro escopo
+    console.log(nome3, nome4);
+}
