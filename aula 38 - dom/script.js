0f061376f7cdd6a7d437537dfe415da8f5d78997
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-02-20 10:29:32 -0300

    Aula 38

---
+++ aula 38 - dom/script.js
@@ -0,0 +1,14 @@
+/**
+ * Window -> é o objeto pai no HTML
+ * document HTML -> seguindo a ordem document filho do window
+ * heade -> filho do document
+ * body -> filho do html
+ * 
+ * header -> Não aparece no modelo mas pode existir
+ * main -> Não aparece no modelo mas pode existir
+ * footer -> Não aparece no modelo mas pode existir
+ * 
+ * section -> filho do body no modelo de exemplo
+ * h1 -> filho do section
+ * conteúdo do h1 -> filho do h1
+ */
