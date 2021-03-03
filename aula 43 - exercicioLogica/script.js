66c782f77c1f2fb7788f984464a1f338e62d0599
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-03-02 22:41:41 -0300

    Aula 43

---
+++ aula 43 - exercicioLogica/script.js
@@ -0,0 +1,16 @@
+/**
+ * Função que recebe 2 números e retorna o maior
+ */
+
+const n1 = 10;
+const n2 = 5;
+
+function maior(x, y) {
+    if (x > y) {
+        console.log(x);
+    } else {
+        console.log(y);
+    }
+}
+
+maior(n1, n2);
