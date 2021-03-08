20fa3f85e756fbd138e88832db85cc6bddab88e0
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-03-08 20:37:35 -0300

    Cria botao apagar

--- aula 48 - listaDeTarefas/scripts/main.js
+++ aula 48 - listaDeTarefas/scripts/main.js
@@ -1,18 +1,46 @@
 const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
-const tarefa = document.querySelector('.tarefa');
+const tarefas = document.querySelector('.tarefas');
 
 function criaLi() {
     const li = document.createElement('li');
     return li;
 }
+
+inputTarefa.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+})
+
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    li.appendChild(botaoApagar);
+}
+
 function criaTarefa(textoInput) {
     const li = criaLi();
     li.innerText = textoInput;
-    tarefa.appendChild(li);
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
 }
 
-btnTarefa.addEventListener('click', function() {
-    if(!inputTarefa.value) return;
+btnTarefa.addEventListener('click', function () {
+    if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
+})
+
+document.addEventListener('click', function(event) {
+    const el = event.target;
+    console.log(el);
 })
