c2eee28d72a89ccd230944ecb173019ba230c9c5
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-03-11 19:36:35 -0300

    Aula 50

--- aula 50 - parametrosDaFuncao/script.js
+++ aula 50 - parametrosDaFuncao/script.js
@@ -2,3 +2,9 @@ function funcao() {
     console.log(arguments);    // Arguments = tudo que foi passado para a função
 }
 funcao('Valor', 1, 2, 3, 4, 5, 6);
+
+/* Se não tiver um paramentro*/
+function funcaoB(a, b = 2, c) { //Caso B não seja passado ele irá valer 2
+    console.log(a + b +c)
+}
+funcaoB(2, undefined, 10);//Unica maneira de pular B e usar o valor padrão
