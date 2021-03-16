14136018a148e270fdd919bbfa5e73a6d1ee53a6
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-03-15 22:10:19 -0300

    Aula 55

---
+++ aula 55 - IIFE/script.js
@@ -0,0 +1,11 @@
+// IIFE -> Immediately invoked function expression
+// função invocada assim que iniciar o programa
+
+(function() {
+    const nome = 'Felipe';
+
+    console.log(nome);
+})();
+
+const nome = 'qualquer coisa';
+console.log(nome);
