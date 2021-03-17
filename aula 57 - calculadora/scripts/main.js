51cc4f5e9cfc64c716c39b48c230f0541855dae0
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-03-17 19:19:38 -0300

    Limpar display

---
+++ aula 57 - calculadora/scripts/main.js
@@ -0,0 +1,37 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+
+        btnClear: document.querySelector('.btn-clear'),
+
+        clearDisplay() {
+            this.display.value = ''; 
+        },
+
+        inicia() {
+            this.cliqueBotoes();
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', event => {
+                const el = event.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+            });
+        },
+        
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
