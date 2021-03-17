6e28ae403ef080c966b550037a6753058d55ed67
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-03-17 19:37:50 -0300

    Aula 57

--- aula 57 - calculadora/scripts/main.js
+++ aula 57 - calculadora/scripts/main.js
@@ -4,28 +4,67 @@ function criaCalculadora() {
 
         btnClear: document.querySelector('.btn-clear'),
 
-        clearDisplay() {
-            this.display.value = ''; 
-        },
-
         inicia() {
             this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+            
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta invalida');
+                    return;
+                }
+
+                this.display.value = conta;
+            } catch (e) {
+                alert('Conta invalida');
+                return;
+            }
         },
 
         cliqueBotoes() {
             document.addEventListener('click', event => {
                 const el = event.target;
 
-                if(el.classList.contains('btn-num')) {
+                if (el.classList.contains('btn-num')) {
                     this.btnParaDisplay(el.innerText);
                 }
 
-                if(el.classList.contains('btn-clear')) {
+                if (el.classList.contains('btn-clear')) {
                     this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
                 }
             });
         },
-        
+
         btnParaDisplay(valor) {
             this.display.value += valor;
         }
